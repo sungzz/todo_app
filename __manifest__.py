d9c4650d3d23c04eadb5e969aebcb6483f62c26a
sung <khisungz@gmail.com>
2018-07-14 12:15:38 +0700

    add new file

---
+++ __manifest__.py
@@ -0,0 +1,7 @@
+{
+    'name': 'To-Do Application',
+    'description': 'Manage your personal To-Do task.',
+    'author': 'Santoni',
+    'depends': ['base'],
+    'application': True,
+}
---
+++ models/todo_model.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields
+class TodoTask(models.Model):
+    _name = 'todo.task'
+    _description = 'To-do Task'
+    name = fields.Char('Desciption', required=True)
+    is_done = fields.Boolean('Done?')
+    active = fields.Boolean ('Active?', default=True)
+
+    
