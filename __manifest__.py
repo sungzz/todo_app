c9da86491b7b4d24cf9021075de14d0bb1978e04
sung <khisungz@gmail.com>
2018-07-15 20:31:52 +0700

    tutorial is done for module todo-app

--- __manifest__.py
+++ __manifest__.py
@@ -2,6 +2,24 @@
     'name': 'To-Do Application',
     'description': 'Manage your personal To-Do task.',
     'author': 'Santoni',
+    'description': """
+This module allows users to create their own notes inside Odoo
+=================================================================
+
+Use notes to write meeting minutes, organize ideas, organize personal todo
+lists, etc. Each user manages his own personal Notes. Notes are available to
+their authors only, but they can share notes to others users so that several
+people can work on the same note in real time. It's very efficient to share
+meeting minutes.
+
+Notes can be found in the 'Home' menu.
+""",
     'depends': ['base'],
     'application': True,
+    'data': [
+        'security/ir.model.access.csv',
+        'security/todo_access_rules.xml',
+        'views/todo_menu.xml',
+        'views/todo_view.xml'
+    ],
 }
--- models/todo_model.py
+++ models/todo_model.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields
+from odoo import models, fields, api
 class TodoTask(models.Model):
     _name = 'todo.task'
     _description = 'To-do Task'
@@ -7,4 +7,14 @@ class TodoTask(models.Model):
     is_done = fields.Boolean('Done?')
     active = fields.Boolean ('Active?', default=True)
 
-    
+    @api.multi
+    def do_toggle_done(self):
+        for task in self:
+            task.is_done = not task.is_done
+        return True    
+
+    @api.model
+    def do_clear_done(self):
+        dones = self.search([('is_done', '=', True)])
+        dones.write({'active': False})
+        return True
